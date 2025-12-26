9b9a129228ea8bc18f622cb34b0b5dc84d5d66e2
kpcyrd <git@rxv.cc>
2025-12-26 01:46:48 +0100

    Blink an LED (didn't work for me)

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,25 @@
 #![no_std]
 #![no_main]
 
-use ch32_hal::{self as hal, delay::Delay};
+use ch32_hal::{
+    self as hal,
+    delay::Delay,
+    gpio::{Level, Output},
+};
 use panic_halt as _;
 use qingke::riscv;
 
 #[qingke_rt::entry]
 fn main() -> ! {
     // hal::debug::SDIPrint::enable();
-    let _pac = hal::init(hal::Config::default());
+    let pac = hal::init(hal::Config::default());
     let mut delay = Delay;
 
     // Setup a counter
     let mut ctr = 0u32;
+
+    // Setup a pin (unfortunately, I couldn't figure out the on-board LED pin)
+    let mut led = Output::new(pac.PD4, Level::Low, Default::default());
 
     loop {
         // Print the counter and increment
@@ -20,7 +27,10 @@ fn main() -> ! {
         ctr = ctr.saturating_add(1);
 
         // Sleep
-        delay.delay_ms(100);
+        delay.delay_ms(500);
+
+        // Toggle LED
+        led.toggle();
 
         // Wait for interrupt
         // riscv::asm::wfi();
